59258c2e7b234062c62595f3b648d31f1d478106
Britodfbr <britodfbr@gmail.com>
2023-02-21 08:33:35 -0300

    feat: tratativas anteriores ao infarto de 2022

--- tests/test_incolumepy_nlp_basic.py
+++ tests/test_incolumepy_nlp_basic.py
@@ -18,15 +18,20 @@ def test_file_exist(entrance):
     ["entrance", "expected"],
     (
         (__version__, True),
+        ("1", False),
+        ("1.0", False),
+        ("0.1", False),
+        ("1.1.1-rc0", False),
+        ("1.1.1-rc-0", False),
+        ("1.0.1-dev0", False),
+        ('1.1.1-a0', False),
+        ('1.1.1-a.0', True),
         ("0.0.1", True),
         ("0.1.0", True),
         ("1.0.0", True),
         ("1.0.1", True),
         ("1.1.1", True),
-        ("1.1.1-rc0", True),
         ("1.1.1-rc.0", True),
-        ("1.1.1-rc-0", True),
-        ("1.0.1-dev0", True),
         ("1.0.1-dev.0", True),
         ("1.0.1-dev.1", True),
         ("1.0.1-dev.2", True),
@@ -35,10 +40,12 @@ def test_file_exist(entrance):
         ("1.0.1-dev.0", True),
         ("1.0.1-beta.0", True),
         ("1.1.1-alpha.99999", True),
-        ("1.1.1-rc.99999", True),
+        ("11111.1.1-rc.99999", True),
         ("1.1.99999", True),
         ("1.999999.1", True),
     ),
 )
 def test_version(entrance, expected):
-    assert re.fullmatch(r"\d\.\d\.\d(-\w+\.\d+)?", __version__, flags=re.I)
+    assert bool(
+        re.fullmatch(r"\d+\.\d+\.\d+(-\w+\.\d+)?", entrance, flags=re.I)
+    ) == expected
